dee06f92e38e24ee4c2676737d1f3689535bea1e
Dymitr Mitrafanau <prefabkalkulator@gmail.com>
2025-11-24 18:37:45 +0100

    fix: Poprawiono nasłuchiwanie na porcie dla Cloud Run (process.env.PORT).

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = parseInt(process.env.PORT) || 8080;
 
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -16,6 +16,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
